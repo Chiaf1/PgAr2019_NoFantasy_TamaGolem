ff706a3766255b8672fda5e68abc9b03d0d72dbf
Daniele <28843028+Chiaf1@users.noreply.github.com>
2019-04-29 11:42:02 +0200

    creazione scheletro grafo

--- src/Grafo/Arco.java
+++ src/Grafo/Arco.java
@@ -1,5 +1,26 @@
 package Grafo;
 
 public class Arco {
+	private Nodo nodo1;
+	private Nodo nodo2;
+	private int valore;
 
+	public Arco(Nodo nodo1, Nodo nodo2, int valore) {
+		this.nodo1 = nodo1;
+		this.nodo2 = nodo2;
+		this.valore = valore;
+	}
+
+	public Nodo getNodo1() {
+		return nodo1;
+	}
+
+	public Nodo getNodo2() {
+		return nodo2;
+	}
+
+	public int getValore() {
+		return valore;
+	}
+	
 }
--- src/Grafo/Grafo.java
+++ src/Grafo/Grafo.java
@@ -1,5 +1,54 @@
 package Grafo;
 
-public class Grafo {
+import java.util.*;
 
+public class Grafo {
+	private ArrayList<Nodo> nodi = new ArrayList<Nodo>();
+	private Map<Integer, Arco> archi = new HashMap<>();
+	private int v;
+	private int n;
+	
+	public Grafo(int v, int n) {
+		
+	}
+
+	public ArrayList<Nodo> getNodi() {
+		return nodi;
+	}
+	
+	public Nodo getNodo(String colore) {
+		return;
+	}
+	
+	public Nodo getNodo(int i) {
+		return;
+	}
+
+	public Map<Integer, Arco> getArchi() {
+		return archi;
+	}
+	
+	public Arco getArco(int i) {
+		return;
+	}
+
+	public int getV() {
+		return v;
+	}
+
+	public int getN() {
+		return n;
+	}
+	
+	private void generaEq() {
+		
+	}
+	
+	private void addArco(Nodo nodo1, Nodo nodo2, int v) {
+		
+	}
+	
+	private void addNodo() {
+		
+	}
 }
